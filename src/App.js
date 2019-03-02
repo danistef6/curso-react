a7698d3b578e03ed30e00960df2841941b14f8c3
Daniela Buitrago <danielabuitrago@MacBook-Pro-de-Daniela.local>
2019-03-02 18:36:06 -0500

    Actividad Dia 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-import MyComponent from './components/myComponent';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import proptypes from 'prop-types';
+import Dashboard from './components/pages/dashboardPage'
+import DetailPage from './components/pages/detailPage'
+import "semantic-ui-css/semantic.min.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <MyComponent greeting = {'Hola mundo desde una prop'} />
-      </div>
+const App = ({location}) => (
+  <div className="ui container" >
+    <Switch>
+      <Route location = {location} path="/detail" exact component={DetailPage} />
+      <Route location = {location} path="/" exact component={Dashboard} />
+    </Switch>
+  </div>
     );
+App.prototype = {
+  location : proptypes.shape({
+      pathname: proptypes.string.isRequired
+  }).isRequired
   }
-}
 
 export default App;
